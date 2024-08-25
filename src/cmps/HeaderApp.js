6c80859a7d77a393b50b4c70c814c98b0083d26d
nimrod aviam <nimrod.a6@gmail.com>
2024-08-25 20:27:40 +0300

    style

--- src/cmps/HeaderApp.js
+++ src/cmps/HeaderApp.js
@@ -8,11 +8,14 @@ const HeaderApp = () => {
   return (
     <nav className="navbar fixed-top bg-dark">
     <div className="container-fluid">
-      <div className="navbar-brand" onClick={() => navigate(`/`)} style={{ cursor: 'pointer', color: '#c6c6c6' }}>
+      <div className="navbar-item" onClick={() => navigate(`/`)} style={{ cursor: 'pointer', color: '#c6c6c6' }}>
         Spoty
       </div>
       <div className="d-flex justify-content-end">
-        <div className="nav-item" onClick={() => navigate(`/favorites`)} style={{ cursor: 'pointer', color: '#c6c6c6' }}>
+      <div className="nav-item" onClick={() => navigate(`/`)} style={{ cursor: 'pointer', color: '#c6c6c6' }}>
+          Home
+        </div>
+        <div className="nav-item ms-4" onClick={() => navigate(`/favorites`)} style={{ cursor: 'pointer', color: '#c6c6c6' }}>
           Favorites 🤍
         </div>
       </div>
