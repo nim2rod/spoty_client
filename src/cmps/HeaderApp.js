82e9aeb06487b22427d1dd6ca9c158a85e057cce
nimrod aviam <nimrod.a6@gmail.com>
2024-08-26 08:01:56 +0300

    toggle light/dark theme

--- src/cmps/HeaderApp.js
+++ src/cmps/HeaderApp.js
@@ -1,26 +1,39 @@
-import React from 'react'
-import {useNavigate} from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 
 const HeaderApp = () => {
   const navigate = useNavigate()
+  const [theme, setTheme] = useState('dark')
+
+  useEffect(() => {
+    document.body.setAttribute('data-theme', theme)
+  }, [theme])
+
+
+  const toggleTheme = (() => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  })
 
   return (
-    <nav className="navbar fixed-top bg-dark">
-    <div className="container-fluid">
-      <div className="navbar-item" onClick={() => navigate(`/`)} style={{ cursor: 'pointer', color: '#c6c6c6' }}>
-        Spoty
-      </div>
-      <div className="d-flex justify-content-end">
-      <div className="nav-item" onClick={() => navigate(`/`)} style={{ cursor: 'pointer', color: '#c6c6c6' }}>
-          Home
+    <nav className="navbar fixed-top header">
+      <div className="container-fluid">
+        <div className="navbar-item" onClick={() => navigate(`/`)} style={{ cursor: 'pointer', color: '#c6c6c6' }}>
+          Spoty
         </div>
-        <div className="nav-item ms-4" onClick={() => navigate(`/favorites`)} style={{ cursor: 'pointer', color: '#c6c6c6' }}>
-          Favorites 🤍
+        <div className="d-flex justify-content-end">
+          <button className="btn" onClick={toggleTheme}>
+            {theme === 'light' ? 'Dark Theme' : 'Light Theme'}
+          </button>
+          <div className="nav-item" onClick={() => navigate(`/`)} style={{ cursor: 'pointer', color: '#c6c6c6' }}>
+            Home
+          </div>
+          <div className="nav-item ms-4" onClick={() => navigate(`/favorites`)} style={{ cursor: 'pointer', color: '#c6c6c6' }}>
+            Favorites 🤍
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
+    </nav>
   );
 }
 
